64a557b2166b01be2a0f7a0a3ee2f4fa2045e68d
anggunpermatsari123 <anggunadex48@gmail.com>
2024-05-22 10:36:27 +0700

    first commit

---
+++ prak10/routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\PasienController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/kabar', function () {
+    return view('kondisi');
+});
+
+Route::get('/pasien', function () {
+    return view('pasien');
+});
+
+Route::get('/admin', [AdminController::class, 'index']);
+
+Route::get('/admin/pasien', [PasienController::class, 'index']);
+
